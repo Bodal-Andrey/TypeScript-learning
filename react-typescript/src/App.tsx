ccb6957f4119a226b8439d618d3e3fa7d24e0ac9
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-11-20 18:24:42 +0500

    add lesson_1

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import './App.css';
 
+type TitleProps = {
+  title: string,
+  test?: string,
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Title = ({ title, test = 'test' }: TitleProps) => <h1>{title}</h1>
+
+const App = () => <Title title="test" />
 
 export default App;
