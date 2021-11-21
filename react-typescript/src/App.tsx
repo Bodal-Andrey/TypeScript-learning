f42f619733735d7f8de0bfd06b2691bd037443ec
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-11-21 15:12:49 +0500

    add lesson_3

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,25 +1,74 @@
 import React from 'react';
 import './App.css';
 
+// type CounterState = {
+//   count: number,
+// };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// type CounterProps = {
+//   readonly title?: string,
+// };
+
+// class Counter extends React.Component<CounterProps, CounterState> {
+//   state = {
+//     count: 0,
+//   }
+
+//   static defaultProps: CounterProps = {
+//     title: "Default counter: ",
+//   }
+
+//   handleClick = (evt: React.MouseEvent<HTMLButtonElement>) => {
+//     console.log(`${evt.clientX}, ${evt.clientY}`)
+//     this.setState(({count}) => ({
+//       count: ++count,
+//     }));
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.props.title}{this.state.count}</h1>
+//         <button onClick={this.handleClick}>+1</button>
+//       </div>
+//     );
+//   }
+// };
+
+class Form extends React.Component<{}, {}> {
+  handleFocus = (evt: React.FocusEvent<HTMLInputElement>) => {
+    console.log(evt.currentTarget);
+  }
+
+  handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    console.log('Submitted!');
+  }
+
+  handleCopy = (evt: React.ClipboardEvent<HTMLInputElement>) => {
+    console.log('Copied!');
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.handleSubmit}
+      >
+        <label>
+          Simple text:
+          <input
+            type="text"
+            name="text"
+            onFocus={this.handleFocus}
+            onCopy={this.handleCopy}
+          />
+          <button type="submit">Submit</button>
+        </label>
+      </form>
+    )
+  }
 }
+
+const App: React.FC = () => <Form />
 
 export default App;
